204ff463d0276923a596e8a48dc3a59aca82d5bf
Kevin <Kevin@Kevin-Laptop>
2014-05-15 17:01:26 +0200

    Committing Design Patterns assignment

---
+++ src/main/java/com/canning/tp2_design_patterns/singleton/MainDriver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.canning.tp2_design_patterns.singleton;
+
+/**
+ *
+ * @author Kevin Canning
+ */
+public class MainDriver {
+    public static void main(String[] args) {
+        SingletonExample singletonExample = SingletonExample.getInstance();
+        singletonExample.sayHello();
+    }
+}
